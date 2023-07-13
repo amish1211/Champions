b9ab278799555c5f9b1877c86076ad5b3c41a43d
Amish Kumar Singh <amish.kumar1211@gmail.com>
2023-07-14 00:54:48 +0530

    achieve stretch goals

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import {
   getDatabase,
   ref,
   push,
-  onValue
+  onValue,
+  get,
+  update,
 } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
 
 const appSettings = {
@@ -16,26 +18,38 @@ const database = getDatabase(app);
 const endorsementInDB = ref(database, "endorsement");
 
 const endorsementTextEl = document.getElementById("endorsement-text");
+const endorsementFromEl = document.getElementById("from");
+const endorsementToEl = document.getElementById("to");
 const publishBtn = document.getElementById("publish-btn");
 const endorsementListEl = document.getElementById("endorsement-list");
 
 publishBtn.addEventListener("click", function () {
-  const endorsementText = endorsementTextEl.value;
-  push(endorsementInDB, endorsementText);
-  clearEndorsementTextEl();
+  const endorsementObj = {
+    from: endorsementFromEl.value,
+    to: endorsementToEl.value,
+    text: endorsementTextEl.value,
+    likeCount: 0,
+    isLiked: false,
+  };
+  push(endorsementInDB, endorsementObj);
+  clearEndorsementInputEls();
 });
 
 onValue(endorsementInDB, function (snapshot) {
   if (snapshot.exists()) {
     clearEndorsementListEl();
-    const endorsementsArray = Object.values(snapshot.val());
-    endorsementsArray.forEach(appendToEndorsementList);
+    const endorsementObjectsArray = Object.values(snapshot.val());
+    const endorsementKeysArray = Object.keys(snapshot.val());
+
+    appendToEndorsementList(endorsementObjectsArray, endorsementKeysArray);
   } else {
     clearEndorsementListEl();
   }
 });
 
-function clearEndorsementTextEl() {
+function clearEndorsementInputEls() {
+  endorsementFromEl.value = "";
+  endorsementToEl.value = "";
   endorsementTextEl.value = "";
 }
 
@@ -43,6 +57,53 @@ function clearEndorsementListEl() {
   endorsementListEl.innerHTML = "";
 }
 
-function appendToEndorsementList(endorsement) {
-  endorsementListEl.innerHTML += `<li>${endorsement}</li>`;
+function appendToEndorsementList(endorsementsArray, endorsementKeysArray) {
+  for (let i = 0; i < endorsementsArray.length; i++) {
+    const endorsementObj = endorsementsArray[i];
+    const endorsementId = endorsementKeysArray[i];
+
+    const endorsementLi = document.createElement("li");
+    endorsementLi.innerHTML += `<p class="from-text">From ${endorsementObj.from}</p> <p>${endorsementObj.text}</p>`;
+    const endorsementFooter = document.createElement("div");
+    endorsementFooter.classList.add("endorsement-footer");
+    endorsementFooter.innerHTML += `<p class="to-text">To ${endorsementObj.to}</p>`;
+    const likeHTML = document.createElement("div");
+    likeHTML.classList.add("like");
+    likeHTML.innerHTML += `&hearts;`;
+    likeHTML.style.color = endorsementObj.isLiked ? "red" : "black";
+    const likeCountHTML = document.createElement("span");
+    likeCountHTML.classList.add("like-count");
+    likeCountHTML.dataset.id = endorsementId;
+    likeCountHTML.textContent = endorsementObj.likeCount;
+    likeHTML.addEventListener("click", function () {
+      const likeCountRef = ref(
+        database,
+        `endorsement/${endorsementId}/likeCount`
+      );
+
+      const isLikedRef = ref(database, `endorsement/${endorsementId}/isLiked`);
+
+      get(isLikedRef).then(function (snapshot) {
+        const isLiked = snapshot.val();
+        get(likeCountRef).then(function (snapshot) {
+          const likeCount = snapshot.val();
+          if (isLiked) {
+            update(ref(database, `endorsement/${endorsementId}`), {
+              likeCount: likeCount - 1,
+              isLiked: false,
+            });
+          } else {
+            update(ref(database, `endorsement/${endorsementId}`), {
+              likeCount: likeCount + 1,
+              isLiked: true,
+            });
+          }
+        });
+      });
+    });
+    likeHTML.append(likeCountHTML);
+    endorsementFooter.append(likeHTML);
+    endorsementLi.append(endorsementFooter);
+    endorsementListEl.append(endorsementLi);
+  }
 }
